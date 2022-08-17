7d4b59864f2dd8948b1e705631a330805a5a9c76
Alex Standiford <a@alexstandiford.com>
2022-08-17 07:06:39 -0400

    WIP

--- lib/Builders/Meta_Query_Builder.php
+++ lib/Builders/Meta_Query_Builder.php
@@ -5,10 +5,11 @@ namespace Underpin\WordPress\Builders;
 use Underpin\Exceptions\Invalid_Field;
 use Underpin\Helpers\Array_Helper;
 use Underpin\Helpers\Object_Helper;
+use Underpin\Interfaces\Can_Convert_To_Instance;
 use Underpin\WordPress\Abstracts\Clause_Builder;
 use WP_Meta_Query;
 
-class Meta_Query_Builder extends Clause_Builder {
+class Meta_Query_Builder extends Clause_Builder implements Can_Convert_To_Instance {
 
 	protected array $clauses = [];
 
@@ -21,6 +22,23 @@ class Meta_Query_Builder extends Clause_Builder {
 		$this->args = Array_Helper::merge( $this->args ?? [], $parameters );
 
 		return $this;
+	}
+
+	/**
+	 * @throws Invalid_Field
+	 */
+	public function to_instance(): WP_Meta_Query {
+		$instance = Object_Helper::make_class( [
+				'class' => $this->instance,
+				'args'  => $this->to_array(),
+			]
+		);
+
+		if ( $instance instanceof WP_Meta_Query ) {
+			return $instance;
+		} else {
+			throw new Invalid_Field( message: 'Instance must be an instance of WP_Meta_Query.', data: [ 'received' => $this->instance ] );
+		}
 	}
 
 }
