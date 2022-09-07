600f1d6a7b6e25c17b1395f086e0ee750eedc846
Alex Standiford <a@alexstandiford.com>
2022-09-07 10:09:15 -0400

    WIP

--- lib/Integrations/Rest/Item.php
+++ lib/Integrations/Rest/Item.php
@@ -8,9 +8,6 @@ use Underpin\Enums\Rest;
 use Underpin\Exceptions\Item_Not_Found;
 use Underpin\Exceptions\Middleware_Exception;
 use Underpin\Exceptions\Operation_Failed;
-use Underpin\Factories\Registry_Items\Param;
-use Underpin\Helpers\Array_Helper;
-use Underpin\Helpers\Processors\Array_Processor;
 use Underpin\Helpers\String_Helper;
 use Underpin\Interfaces\Feature_Extension;
 use Underpin\Interfaces\Identifiable;
@@ -18,6 +15,7 @@ use Underpin\Interfaces\Loader_Item;
 use Underpin\Registries\Controller;
 use Underpin\Abstracts\Rest_Action;
 use Underpin\WordPress\Adapters\WP_Rest_Request_To_Request_Adapter;
+use ValueError;
 use WP_Error;
 use WP_REST_Request;
 use WP_REST_Response;
@@ -43,45 +41,16 @@ class Item implements Feature_Extension, Identifiable, Loader_Item {
 		return String_Helper::prepend( implode( '/', $result ), '/' );
 	}
 
-	protected function get_registration_args(): array {
-		if ( ! isset( $this->registration_args ) ) {
-			$this->registration_args = ( new Array_Processor( $this->controller->to_array() ) )->each( fn ( $value, $key ) => [
-				'methods'             => $key,
-				'callback'            => [ $this, 'get_response' ],
-				'permission_callback' => [ $this, 'middleware' ],
-			] )->to_array();
-		}
-
-		return $this->registration_args;
-	}
-
 	public function register_routes() {
-		register_rest_route( $this->namespace, $this->get_route(), [
-			$this->get_registration_args(),
-			'schema' => [ $this, 'get_schema' ],
-		] );
-	}
-
-	/**
-	 * @throws Middleware_Exception
-	 */
-	public function get_schema( WP_REST_Request $request ) {
-		$action = $this->get_action( $request );
-		$method = $action->get_request()->get_method()->value;
-
-		if ( ! isset( $this->schemas[ $method ] ) ) {
-			$this->schemas[ $method ] = [
-				'$schema'    => 'http://json-schema.org/draft-04/schema#',
-				'title'      => $this->controller->name,
-				'type'       => 'object',
-				'properties' => Array_Helper::each( $action->get_signature(), fn ( Param $param, string $key ) => [
-					'description' => $param->description,
-					'type'        => $param->get_type()->value,
-				] ),
-			];
+		foreach ( $this->controller->to_array() as $method => $action ) {
+			register_rest_route( $this->namespace, $this->get_route(), [
+				[
+					'methods'             => $method,
+					'callback'            => [ $this, 'get_response' ],
+					'permission_callback' => [ $this, 'middleware' ],
+				],
+			] );
 		}
-
-		return $this->schemas[ $method ];
 	}
 
 	public function do_actions(): void {
@@ -92,8 +61,10 @@ class Item implements Feature_Extension, Identifiable, Loader_Item {
 	 * Gets the actual action for the request.
 	 *
 	 * @throws Middleware_Exception
+	 * @throws ValueError
 	 */
 	protected function get_action( WP_REST_Request $request ): Rest_Action {
+
 		if ( ! isset( $this->action ) ) {
 			try {
 				try {
@@ -141,6 +112,8 @@ class Item implements Feature_Extension, Identifiable, Loader_Item {
 			$this->get_action( $request )->do_middleware_actions();
 		} catch ( Middleware_Exception $exception ) {
 			return new WP_Error( $exception->getCode(), $exception->getMessage() );
+		} catch ( ValueError $e ) {
+			// Let WordPress deal with unknown request types.
 		}
 
 		return true;
