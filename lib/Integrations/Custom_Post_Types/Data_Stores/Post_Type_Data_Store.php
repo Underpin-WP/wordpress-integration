04af0fbfd623384744d5711371a30ee450c1397c
Alex Standiford <a@alexstandiford.com>
2022-10-24 09:44:32 -0400

    Progress

--- lib/Integrations/Custom_Post_Types/Data_Stores/Post_Type_Data_Store.php
+++ lib/Integrations/Custom_Post_Types/Data_Stores/Post_Type_Data_Store.php
@@ -8,6 +8,7 @@ use Underpin\Factories\Log_Item;
 use Underpin\Interfaces\Can_Create;
 use Underpin\Interfaces\Can_Delete;
 use Underpin\Interfaces\Can_Update;
+use Underpin\Interfaces\Data_Store;
 use Underpin\Interfaces\Identifiable_Int;
 use Underpin\Interfaces\Model;
 use Underpin\Registries\Logger;
@@ -16,7 +17,7 @@ use Underpin\WordPress\Exceptions\Post_Delete_Canceled;
 use Underpin\WordPress\Exceptions\Post_Delete_Failed;
 use WP_Post;
 
-class Post_Type_Data_Store implements Can_Create, Can_Update, Can_Delete {
+class Post_Type_Data_Store implements Can_Create, Can_Update, Can_Delete, Data_Store {
 
 	/**
 	 * @param class-string<Model_To_WP_Post_Adapter> $adapter
